3949a03a2111e3c3344f9ea3f253351bc3af2a28
ndido98 <ndido98@gmail.com>
2025-04-30 15:56:31 +0200

    edit models to use auth user

--- polls/models.py
+++ polls/models.py
@@ -6,41 +6,31 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
-
-class User(models.Model):
-    username = models.CharField(unique=True, max_length=100)
-    password = models.CharField(max_length=255)
-
-    class Meta:
-        managed = False
-        db_table = 'user'
+from django.contrib.auth.models import User
 
 
 class Poll(models.Model):
     question = models.CharField(max_length=255)
+    voters = models.ManyToManyField(User, through='Vote', related_name='polls')
 
     class Meta:
-        managed = False
         db_table = 'poll'
 
 
 class PollOption(models.Model):
-    poll = models.ForeignKey(Poll, models.DO_NOTHING)
+    poll = models.ForeignKey(Poll, on_delete=models.DO_NOTHING, related_name='options')
     option_text = models.CharField(max_length=255)
 
     class Meta:
-        managed = False
         db_table = 'poll_option'
 
 
 class Vote(models.Model):
     pk = models.CompositePrimaryKey('poll_id', 'user_id')
-    poll = models.ForeignKey(Poll, models.DO_NOTHING)
-    option = models.ForeignKey(PollOption, models.DO_NOTHING)
-    user = models.ForeignKey(User, models.DO_NOTHING)
+    poll = models.ForeignKey(Poll, on_delete=models.DO_NOTHING, related_name='votes')
+    option = models.ForeignKey(PollOption, on_delete=models.DO_NOTHING, related_name='votes')
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
     class Meta:
-        managed = False
         db_table = 'vote'
         unique_together = (('poll', 'user'),)
