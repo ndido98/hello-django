5f76526c65d82150facc305caa5d7abbe5c4904b
ndido98 <ndido98@gmail.com>
2025-05-06 13:20:32 +0200

    add authenticated features

--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
     path('<int:poll_id>/', views.detail, name='detail'),
+    path('<int:poll_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.db import models
+from django.contrib.auth import authenticate, login as auth_login
 
-from polls.models import Poll
+from polls.models import Poll, PollOption, Vote
 
 # Create your views here.
 def index(request):
@@ -13,3 +14,40 @@ def detail(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
     options_with_votes = poll.options.annotate(vote_count=models.Count('votes'))
     return render(request, 'polls/detail.html', {'poll': poll, 'options': options_with_votes})
+
+
+def vote(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    options_with_votes = poll.options.annotate(vote_count=models.Count('votes'))
+    if request.method == "POST":
+        try:
+            selected_option = poll.options.get(pk=request.POST['option'])
+        except (KeyError, PollOption.DoesNotExist):
+            return render(request, 'polls/detail.html', {
+                'poll': poll,
+                'options': options_with_votes,
+                'error': 'You did not select a valid option.'
+            })
+        else:
+            Vote.objects.update_or_create(
+                poll=poll,
+                user=request.user,
+                defaults={'option': selected_option}
+            )
+    return redirect('polls:detail', poll_id=poll_id)
+
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'polls/login.html')
+    elif request.method == "POST":
+        try:
+            username, password = request.POST['username'], request.POST['password']
+        except KeyError:
+            return render(request, 'polls/login.html', {'error': 'Please enter username and password.'})
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('polls:index')
+        else:
+            return render(request, 'polls/login.html', {'error': 'Invalid username or password.'})
